3317282a0998409eb2364371d737c136b0fec59c
devneto <miguel.neto320@gmail.com>
2022-10-29 23:52:30 -0300

    feat: ensure users add first route

--- src/app.py
+++ src/app.py
@@ -1,6 +1,13 @@
-from flask import Flask 
+from flask import Flask, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config['MONGO_URI']='mongodb://localhost/pymongopoc'
+mongo = PyMongo(app)
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    return {"message":"ok"}
 
 if __name__ == "__main__":
     app.run(debug=True)
